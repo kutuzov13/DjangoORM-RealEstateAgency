54f6781d77ad2a7a59011d81956fd6be9f1321a0
kutuzov13 <kutuzov-1313@yandex.ru>
2021-05-30 14:06:26 +0300

    Add list_filter for SearchFlat

--- property/admin.py
+++ property/admin.py
@@ -8,6 +8,7 @@ class SearchFlat(admin.ModelAdmin):
     readonly_fields = ['created_at']
     list_display = ('address', 'price', 'new_building', 'construction_year', 'town')
     list_editable = ('new_building',)
+    list_filter = ['floor', 'rooms_number', 'has_balcony', 'new_building']
 
 
 admin.site.register(Flat, SearchFlat)
