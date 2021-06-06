a29402b5b98706ca8a62b2a9f199a187f6d211ba
kutuzov13 <kutuzov-1313@yandex.ru>
2021-06-06 13:52:37 +0300

    add filed  owner_pure_phone

--- property/admin.py
+++ property/admin.py
@@ -7,7 +7,7 @@ from .models import Flat, Complaint
 class SearchFlat(admin.ModelAdmin):
     search_fields = ['town', 'address', 'owners_phonenumber', ]
     readonly_fields = ['created_at']
-    list_display = ('address', 'price', 'new_building', 'construction_year', 'town')
+    list_display = ['address', 'price', 'new_building', 'construction_year', 'town', 'owner_pure_phone']
     list_editable = ('new_building',)
     list_filter = ['floor', 'rooms_number', 'has_balcony', 'new_building']
     raw_id_fields = ['liked_by']
